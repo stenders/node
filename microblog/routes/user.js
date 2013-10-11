c6e7fa1e05f0e0ca517b6c1815994a7f7fb277e5
stenders <stenders@163.com>
2013-10-11 21:52:18 +0800

    add express

--- microblog/routes/user.js
+++ microblog/routes/user.js
@@ -2,7 +2,9 @@
 /*
  * GET users listing.
  */
-
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+exports.names = function(req, res){
+  res.send('hello, ' + req.params.username)
+}
