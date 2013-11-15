1d71df000a81cadec26fa0c51bf7ee9650368457
stenders <stenders@163.com>
2013-11-15 19:07:23 +0800

    add blog

--- chunhang/app.js
+++ chunhang/app.js
@@ -5,17 +5,15 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+// app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -27,8 +25,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- chunhang/routes/index.js
+++ chunhang/routes/index.js
@@ -3,6 +3,16 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+module.exports = function(app){
+  app.get('/', function(req, res){
+    res.render('index', {title: 'Blog'})
+  })
+
+  app.get('/login', function(req, res){
+    res.render('login', {title: 'login'})
+  })
+
+  app.get('/reg', function(req, res){
+    res.render('reg', {title: 'Register'})
+  })
+}
